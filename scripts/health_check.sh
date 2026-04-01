de6f37f77a57a3d8232734bb3f1f2cf0fc3ca84e
Vansh <92663795+vansh-deploy@users.noreply.github.com>
2026-04-01 06:00:51 +0530

    Add health check script

---
+++ scripts/health_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Health Check Script
+# Date: 2026-04-01 00:29:57 UTC
+# Author: vansh-deploy
+
+# Placeholder for actual health check commands
+
+echo "Running health checks..."
+
+echo "All systems are operational!"
